7af3eeacfb95369677302463ea46d056cefe57d6
Youssef <joe-saleh13@hotmail.com0>
2025-02-07 09:38:52 +0200

    init

---
+++ src/main/java/com/ahm/erp/erp_manager/ErpManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ahm.erp.erp_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpManagerApplication.class, args);
+	}
+
+}
